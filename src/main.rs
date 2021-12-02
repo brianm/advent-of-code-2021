d2c8cae5f2d0200c1a06523dccdc6f8e015a5417
Brian McCallister <brianm@skife.org>
2021-12-01 20:27:53 -0800

    prettier without the tuples for #2

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,22 @@ fn main() -> Result<()> {
     let one = nums.iter().tuple_windows().filter(|(a, b)| b > a).count();
     println!("{}", one);
 
-
-
     // 1 2 3
     //   2 3 4
-    //     3 4 5   
+    //     3 4 5
     // so 3tuple - next 3tuple is t[3] - t[0] for each 4tuple
     // 1 2 3 4
     //   2 3 4 5
-    //     3 4 5 6    
-    let two = nums
+    //     3 4 5 6
+    let three = nums.windows(4).filter(|w| w[3] > w[0]).count();
+    println!("{}", three);
+
+    let two2 = nums
         .iter()
-        .tuple_windows::<(_, _, _, _)>()        
+        .tuple_windows::<(_, _, _, _)>()
         .filter(|(a, _, _, b)| b > a)
         .count();
-    println!("{}", two);
+    println!("{}", two2);
 
     Ok(())
 }
