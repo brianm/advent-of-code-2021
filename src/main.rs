a87c7a0f3a0f4b1b497ac657409dad12cb162765
Brian McCallister <brianm@skife.org>
2021-12-01 20:23:40 -0800

    day 1

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    let nums: Vec<u32> = include_str!("input")
+        .lines()
+        .map(|n| n.parse().unwrap())
+        .collect();
+
+    let one = nums.iter().tuple_windows().filter(|(a, b)| b > a).count();
+    println!("{}", one);
+
+
+
+    // 1 2 3
+    //   2 3 4
+    //     3 4 5   
+    // so 3tuple - next 3tuple is t[3] - t[0] for each 4tuple
+    // 1 2 3 4
+    //   2 3 4 5
+    //     3 4 5 6    
+    let two = nums
+        .iter()
+        .tuple_windows::<(_, _, _, _)>()        
+        .filter(|(a, _, _, b)| b > a)
+        .count();
+    println!("{}", two);
+
+    Ok(())
+}
